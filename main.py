0da7439ba243c02cccfaab5e524cfd53575cd0f0
georgyteterin <teterin.normal@gmail,com>
2023-08-25 17:28:02 +0300

    добавил скачивание по ссылке

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+import requests
 import uvicorn
 
 
@@ -28,6 +29,28 @@ def download_file_by_name(DDD, YY, n_or_g):
     filename = "brdc" + DDD + "0." + YY + n_or_g + ".jpg"
     return FileResponse(path=filename, filename=filename)
 
+
+@app.get("/link/{num}")
+def download_by_link(num: int):
+    if num == 1:
+        url = 'https://rockweek.ru/wp-content/uploads/2016/09/kiss_44.jpg'
+    elif num == 2:
+        url = 'https://wallpapers.com/images/hd/cute-cats-pictures-ofp9qyt72qck6jqg.jpg'
+    elif num == 3:
+        url = 'https://schoharierecovery.org/wp-content/uploads/2020/09/2-2.jpg'
+    else:
+        return {"ссылка пока не добавлена"}
+    # url = ['https://rockweek.ru/wp-content/uploads/2016/09/kiss_44.jpg',
+    #        'https://wallpapers.com/images/hd/cute-cats-pictures-ofp9qyt72qck6jqg.jpg',
+    #        'https://schoharierecovery.org/wp-content/uploads/2020/09/2-2.jpg'
+    #        ]
+    # r = requests.get(url[num - 1])
+    r = requests.get(url)
+    with open(r'C:\Users\гоша\Documents\my_photos\picture.jpg', 'wb') as f:
+        f.write(r.content)
+    path = r'C:\Users\гоша\Documents\my_photos\picture.jpg'
+    filename = 'picture.jpg'
+    return FileResponse(path=path, filename=filename)
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
