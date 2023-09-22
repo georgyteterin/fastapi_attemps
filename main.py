b60d9865cd6e73bf941d73c210d9b7d0cb98c069
unknown <teterin.normal@gmail.com>
2023-09-22 22:48:41 +0300

    1

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 import requests
 import uvicorn
-
+import gzip
+import shutil
 
 
 app = FastAPI(
-    title = "I AM TRYING"
+    title="I AM TRYING"
 )
+
 
 # ЭТО ЧТОБЫ ПРОВЕРИТЬ ФОРМИРОВАНИЕ ДАТЫ ДЛЯ СКАЧИВАНИЯ ФАЙЛА ПО ЗАПРОСУ http://localhost:8000/filename/213-23-n
 # (естественно и дату и год и n/g можно менят, это из примера из воровского документа по эфемеридам))
 @app.get("/filename/{DDD}-{YY}-{n_or_g}")
 def filename(DDD, YY, n_or_g):
-    filename = "brdc"+DDD+"0."+YY+n_or_g + ".txt"
+    filename = "brdc" + DDD + "0." + YY + n_or_g + ".txt"
     return {"зесь должно быть скачивание файла": filename}
 
 
@@ -21,7 +23,7 @@ def filename(DDD, YY, n_or_g):
 # у меня катинка лежит в той же папке, что и проект, так что адрес короткий
 @app.get("/download_file")
 def download_file():
-  return FileResponse(path='cute cat.jpg', filename='картинка кота.jpeg')
+    return FileResponse(path='cute cat.jpg', filename='картинка кота.jpeg')
 
 
 @app.get("/download_file/{DDD}-{YY}-{n_or_g}")
@@ -57,8 +59,67 @@ def download_by_link(num: int):
     return FileResponse(path=path, filename=filename)
 
 
+@app.get('/download_ephemers')
+def get_file():
+    class SessionWithHeaderRedirection(requests.Session):
+
+        AUTH_HOST = 'urs.earthdata.nasa.gov'
+
+        def __init__(self, username, password):
+
+            super().__init__()
+
+            self.auth = (username, password)
+
+        def rebuild_auth(self, prepared_request, response):
+            headers = prepared_request.headers
+            url = prepared_request.url
+
+            if 'Authorization' in headers:
+
+                original_parsed = requests.utils.urlparse(response.request.url)
+
+                redirect_parsed = requests.utils.urlparse(url)
+
+                if ((original_parsed.hostname != redirect_parsed.hostname) and
+                        redirect_parsed.hostname != self.AUTH_HOST and
+                        original_parsed.hostname != self.AUTH_HOST):
+                    del headers['Authorization']
+            return
+
+    username = "rokja"
+    password = "Par0lephemers!"
+
+    session = SessionWithHeaderRedirection(username, password)
+
+    url = "https://cddis.nasa.gov/archive/gnss/data/daily/2023/brdc/brdc2050.23n.gz"
+
+    filename = url[url.rfind('/') + 1:]
+
+    try:
+
+        response = session.get(url, stream=True)
+        print(response.status_code)
+        response.raise_for_status()
+        with open(filename, 'wb') as fd:
+
+            for chunk in response.iter_content(chunk_size=1024 * 1024):
+                fd.write(chunk)
+
+    except requests.exceptions.HTTPError as e:
+        print(e)
+
+    def rasarch(archive_name):
+        local_filename = archive_name.removesuffix('.gz')
+        with gzip.open(archive_name, 'rb') as file_in:
+            with open(local_filename, 'wb') as file_out:
+                shutil.copyfileobj(file_in, file_out)
+                return(local_filename)
+
+    our_filename = rasarch(filename)
+    return FileResponse(path=our_filename, filename=our_filename)
+
 
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
-
